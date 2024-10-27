be1fe02c981fead1650ec7e92902e2b449ea2de6
Arpon <a4arpon@gmail.com>
2024-10-27 10:34:23 +0600

    base path added on container router types

--- src/hot-shot.ts
+++ src/hot-shot.ts
@@ -218,7 +218,7 @@ export function routerFactory(routers: Array<RouterConstructor>): Hono {
 |
 */
 
-export function routerContainer(routers: Array<() => Hono>, {basePath}: { basePath?: string }): Hono {
+export function routerContainer({basePath, routers}: { basePath?: string, routers: Array<() => Hono>, }): Hono {
     const containerInstance = new Hono().basePath(basePath ?? "")
 
     for (const router of routers) {
